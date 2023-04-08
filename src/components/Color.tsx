8fba8ae2368878013f8f2b3ccbd7a7076158b142
Dima Vyshniakov <morewings@gmail.com>
2023-04-08 22:32:53 +0200

    fix css

--- src/components/Color.tsx
+++ src/components/Color.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import { styled, themes, convert } from "@storybook/theming";
+import { styled } from "@storybook/theming";
 
 const gradient =
   "linear-gradient(-45deg, transparent, transparent 45%, pink 45%, pink 55%, transparent 55%, transparent 100%)";
@@ -11,6 +11,6 @@ export const Color: FC<{ colorName?: string }> = ({ colorName = gradient }) => {
 export const ColorDiv = styled.div({
   width: "16px",
   height: "16px",
-  "border-radius": "8px",
+  borderRadius: "8px",
   border: "1px solid lightgray",
 });

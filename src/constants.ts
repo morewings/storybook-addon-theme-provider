d447f4e7e8520105e95ae6f7ab347ae57737fdef
Dima Vyshniakov <morewings@gmail.com>
2023-04-10 14:11:23 +0200

    clean up

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,3 @@
 export const ADDON_ID = "storybook/theme-provider";
 export const TOOL_ID = `${ADDON_ID}/tool`;
 export const PARAM_KEY = `myAddonParameter`;
-
-export const EVENTS = {
-  RESULT: `${ADDON_ID}/result`,
-  REQUEST: `${ADDON_ID}/request`,
-  CLEAR: `${ADDON_ID}/clear`,
-};
